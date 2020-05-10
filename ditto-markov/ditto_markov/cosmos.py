69981c438064b3ad9a609223b8b76b5d6949d261
Ridwan Hoq <rhoq@brighthealthplan.com>
2020-05-10 15:58:38 -0500

    pep8

--- ditto-markov/ditto_markov/cosmos.py
+++ ditto-markov/ditto_markov/cosmos.py
@@ -4,31 +4,33 @@ from .config import COSMOS_ACCOUNT_URI, COSMOS_ACCOUNT_KEY, COSMOS_DATABASE_ID
 
 events_container_id = 'slack-events'
 
+
 class DittoCosmos:
-  def __init__(self, cosmos_account_uri, cosmos_account_key, cosmos_database_id):
-    self.cosmos = cosmos_client.CosmosClient(cosmos_account_uri, {'masterKey': cosmos_account_key})
-    self.database_id = cosmos_database_id
+    def __init__(self, cosmos_account_uri, cosmos_account_key, cosmos_database_id):
+        self.cosmos = cosmos_client.CosmosClient(cosmos_account_uri, {'masterKey': cosmos_account_key})
+        self.database_id = cosmos_database_id
 
-  def get_container_path(self, container_id):
-    return f"dbs/{self.database_id}/colls/{container_id}"
+    def get_container_path(self, container_id):
+        return f"dbs/{self.database_id}/colls/{container_id}"
 
-  def get_message_events_for_user(self, user):
-    query = """
+    def get_message_events_for_user(self, user):
+        query = """
       SELECT * 
       FROM events e
       WHERE e.user = @user
       AND e.type = "message"
       ORDER BY e.ts DESC
     """
-    return self.cosmos.QueryItems(
-      self.get_container_path(events_container_id), 
-      {
-        'query': query,
-        'parameters': [
-            {'name': '@user', 'value': user}
-        ]
-      },
-      partition_key=user
-    )
-  
+        return self.cosmos.QueryItems(
+            self.get_container_path(events_container_id),
+            {
+                'query': query,
+                'parameters': [
+                    {'name': '@user', 'value': user}
+                ]
+            },
+            partition_key=user
+        )
+
+
 cosmos = DittoCosmos(COSMOS_ACCOUNT_URI, COSMOS_ACCOUNT_KEY, COSMOS_DATABASE_ID)
