31ae2b207f8355cfceae23e84aeeea92eaad157f
Ridwan Hoq <rhoq@brighthealthplan.com>
2020-05-10 16:00:11 -0500

    pep8 (#12)

--- ditto-markov/ditto_markov/cli.py
+++ ditto-markov/ditto_markov/cli.py
@@ -3,20 +3,21 @@ import sys
 from .markov import get_model
 
 team_ayy_lmao_users = {
-  'd': 'U0316HMAS',
-  'tom': 'U034S6QF6',
-  'austin': 'U03169VDW',
-  'rid': 'U0314Q64M',
-  'thomas': 'U031D6P73',
-  'matty': 'U03LTN7RH',
-  'beej': 'U03JA1HKP',
-  'nickpray': 'U09BC6LR5',
-  'partypat': 'U22RC4XB2'
+    'd': 'U0316HMAS',
+    'tom': 'U034S6QF6',
+    'austin': 'U03169VDW',
+    'rid': 'U0314Q64M',
+    'thomas': 'U031D6P73',
+    'matty': 'U03LTN7RH',
+    'beej': 'U03JA1HKP',
+    'nickpray': 'U09BC6LR5',
+    'partypat': 'U22RC4XB2'
 }
 
+
 def main():
-  user_name = sys.argv[1]
-  user = team_ayy_lmao_users[user_name]
-  model = get_model(user)
-  for i in range(20):
-    print(model.make_sentence())
+    user_name = sys.argv[1]
+    user = team_ayy_lmao_users[user_name]
+    model = get_model(user)
+    for i in range(20):
+        print(model.make_sentence())
--- ditto-markov/ditto_markov/markov.py
+++ ditto-markov/ditto_markov/markov.py
@@ -1,37 +1,39 @@
+import markovify
 from azure.core.exceptions import ResourceNotFoundError
 from azure.storage.blob import BlobServiceClient
-import markovify
 
-from .cosmos import cosmos
 from .config import AZURE_STORAGE_CONNECTION_STRING
+from .cosmos import cosmos
 
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 container = "markov"
 
-def get_model(user):
-  cached_model = get_cached_model(user)
-  if cached_model:
-    return cached_model
-  
-  messages = [item["text"] for item in cosmos.get_message_events_for_user(user)]
-  newline_delimited_messages = "\n".join(messages)
-  model = markovify.NewlineText(newline_delimited_messages)
-  write_model_to_blob_storage(user, model.to_json())
 
-  return model
+def get_model(user):
+    cached_model = get_cached_model(user)
+    if cached_model:
+        return cached_model
+
+    messages = [item["text"] for item in cosmos.get_message_events_for_user(user)]
+    newline_delimited_messages = "\n".join(messages)
+    model = markovify.NewlineText(newline_delimited_messages)
+    write_model_to_blob_storage(user, model.to_json())
+
+    return model
+
 
 def get_cached_model(user):
-  try:
-    blob_client = blob_service_client.get_blob_client(container=container, blob=user)
-    model_json = blob_client.download_blob().readall()
-    print(f"cache hit for {user}")
-    return markovify.NewlineText.from_json(model_json)
-  except ResourceNotFoundError:
-    print(f"cache miss for {user}")
-    return None
+    try:
+        blob_client = blob_service_client.get_blob_client(container=container, blob=user)
+        model_json = blob_client.download_blob().readall()
+        print(f"cache hit for {user}")
+        return markovify.NewlineText.from_json(model_json)
+    except ResourceNotFoundError:
+        print(f"cache miss for {user}")
+        return None
+
 
 def write_model_to_blob_storage(user, model):
-  blob_client = blob_service_client.get_blob_client(container=container, blob=user)
-  print(f"uploading blob for {user}")
-  blob_client.upload_blob(model, overwrite=True)
-  
+    blob_client = blob_service_client.get_blob_client(container=container, blob=user)
+    print(f"uploading blob for {user}")
+    blob_client.upload_blob(model, overwrite=True)
