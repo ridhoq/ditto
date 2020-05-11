36a269373b2ce73f0cb81edbd3e5c114f0afdf2a
Ridwan Hoq <rhoq@brighthealthplan.com>
2020-05-11 00:37:00 -0500

    cleanup

--- ditto-markov/tests/test_cosmos.py
+++ ditto-markov/tests/test_cosmos.py
@@ -1,13 +1,5 @@
 from ditto_markov.cosmos import cosmos
 
 
-# @pytest.fixture(scope="module", autouse=True)
-# def my_fixture():
-#     print ('INITIALIZATION')
-#     cosmos = DittoCosmos(COSMOS_ACCOUNT_URI, COSMOS_ACCOUNT_KEY, COSMOS_DATABASE_ID)
-#     print ('TEAR DOWN')
-    
-
 def test_cosmos_client():
     assert cosmos is not None
-
