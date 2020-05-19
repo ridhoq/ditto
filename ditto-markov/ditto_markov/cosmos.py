f04eab47974c69a7f1b9284a7bcb303f9d50109f
Ridwan Hoq <rhoq@brighthealthplan.com>
2020-05-19 00:45:57 -0500

    cosmos tests

--- ditto-markov/ditto_markov/cosmos.py
+++ ditto-markov/ditto_markov/cosmos.py
@@ -1,6 +1,5 @@
 import azure.cosmos.cosmos_client as cosmos_client
 import azure.cosmos.documents as documents
-import urllib3
 from urllib3.util.retry import Retry
 
 from .config import COSMOS_ACCOUNT_URI, COSMOS_ACCOUNT_KEY, COSMOS_DATABASE_ID, COSMOS_DISABLE_TLS
@@ -10,19 +9,19 @@ events_container_id = 'slack-events'
 
 class DittoCosmos:
     def __init__(self, cosmos_account_uri, cosmos_account_key, cosmos_database_id, cosmos_disable_tls=False):
-        print(f"cosmos_disable: {cosmos_disable_tls}")
         connection_policy = documents.ConnectionPolicy()
-        connection_policy.SSLConfiguration = documents.SSLConfiguration()
         connection_policy.ConnectionRetryConfiguration = Retry(
             total=5,
             connect=5,
             backoff_factor=0.1,
         )
+
         if cosmos_disable_tls:
-            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+            connection_policy.SSLConfiguration = documents.SSLConfiguration()
             connection_policy.SSLConfiguration.SSLCaCerts = False
+
         self.cosmos = cosmos_client.CosmosClient(cosmos_account_uri, {'masterKey': cosmos_account_key},
-                                                 connection_policy, )
+                                                 connection_policy)
         self.database_id = cosmos_database_id
 
     def get_container_path(self, container_id):
