346179b0dce3e038fcce55560e0fb2482c87f86f
Ridwan Hoq <rhoq@brighthealthplan.com>
2020-06-06 03:23:52 -0500

    wire up api to return transformed sentences

--- ditto-markov/ditto_markov/api.py
+++ ditto-markov/ditto_markov/api.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from cachetools import LFUCache
+
+from .cache import CacheRepository, BlobCache
+from .markov import get_model
 
 api = FastAPI()
 
+cache_repo = CacheRepository()
 
-@api.get("/")
-def read_root():
-    return {"Hello": "World"}
+lfu_cache = LFUCache(maxsize=5)
+cache_repo.register_cache("lfu", lfu_cache)
+
+blob_cache = BlobCache()
+cache_repo.register_cache("blob", blob_cache)
+
+
+@api.get("/transform/{user}")
+def get_transform(user):
+    model = get_model(user, cache_repo)
+    return {user: model.make_sentence()}
